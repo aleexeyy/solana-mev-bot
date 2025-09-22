7496849a3d5f013ea2b351e07108d8eab61bc8dc
Alex Radchenko <radchenko.2006.3@gmail.com>
2025-09-22 23:54:56 +0200

    feature:implement cycle detection algorithm

--- src/graph.rs
+++ src/graph.rs
@@ -1,7 +1,6 @@
+use core::num;
 use std::{
-    collections::HashMap,
-    fs::{read_dir, read_to_string},
-    str::FromStr,
+    cmp::min, collections::{HashMap, HashSet}, f32::NEG_INFINITY, fs::{read_dir, read_to_string}, str::FromStr
 };
 
 use solana_sdk::pubkey::Pubkey;
@@ -10,22 +9,23 @@ use tracing::{info, warn};
 use crate::bootstrap::pool_schema::{
     DexType, PoolInfo, PoolType, PoolUpdate, StoredPools, TokenInfo,
 };
-use anyhow::{Result, anyhow};
+use anyhow::{anyhow, Context, Result};
+use ethnum::U256;
 
 #[allow(dead_code)]
 #[derive(Debug)]
-struct Node {
+pub struct Node {
     address: Pubkey,
     decimals: u8,
     name: String,
-    symbol: String,
+    pub symbol: String,
 }
 
 #[allow(dead_code)]
 #[derive(Debug)]
-struct Edge {
+pub struct Edge {
     //static fields
-    address: Pubkey,
+    pub address: Pubkey,
     fee_rate: u32,
     pool_type: PoolType,
     dex: DexType,
@@ -33,21 +33,70 @@ struct Edge {
     token_vault_lowest: Pubkey,  // lowest index
     token_vault_highest: Pubkey, // highest index
     config: Pubkey,
+    node_lowest: usize,
+    node_highest: usize,
+    decimals_lowest: u8,
+    decimals_highest: u8,
+    pub reversed: bool,
 
     //dynamic fields
-    sqrt_price: Option<u128>,
+    pub sqrt_price: Option<u128>,
     liquidity: Option<u128>,
     current_tick_index: Option<i32>,
 }
 
+impl Edge {
+    pub fn get_log_exchange_rate(&self, direct: bool) -> f64 {
+        -self.get_exchange_rate(direct).log10()
+    }
+
+    pub fn get_exchange_rate(&self, direct: bool) -> f64 {
+        let decimals_diff: i32 = if self.reversed {
+            self.decimals_highest as i32 - self.decimals_lowest as i32
+        } else {
+            self.decimals_lowest as i32 - self.decimals_highest as i32
+        };
+        let denominator = 10f64.powi(decimals_diff);
+
+        let scaled_price: U256 = U256::from(self.sqrt_price.unwrap());
+        let squared: U256 = scaled_price * scaled_price;
+
+        let high: U256 = squared >> 128;
+        let low: U256 = squared & U256::from(u128::MAX);
+        let price_f64 = high.as_u128() as f64 * 2f64.powi(64) + low.as_u128() as f64;
+
+        let price_f64 = price_f64 / 2f64.powi(128);
+
+        let exchange_rate = price_f64 * denominator;
+
+        if self.reversed == direct {
+            1.0 / exchange_rate    
+        } else {
+            exchange_rate
+        }
+    }
+
+
+    fn get_other_node(&self, this_token: usize) -> Option<usize> {
+        if this_token == self.node_lowest {
+            return Some(self.node_highest);
+        } else if this_token == self.node_highest {
+            return Some(self.node_lowest);
+        } else {
+            return None;
+        }
+    }
+
+}
+
 #[derive(Debug, Default)]
 pub struct Graph {
-    nodes: Vec<Node>,
+    pub nodes: Vec<Node>,
     address_to_node: HashMap<Pubkey, usize>,
-    // adjacency: HashMap<usize, HashSet<usize>>,
-    edges: Vec<Edge>,
+    adjacency: HashMap<usize, HashSet<usize>>, // adjacent pools to the token
+    pub edges: Vec<Edge>,
     address_to_edge: HashMap<Pubkey, usize>,
-    // edge_to_nodes: HashMap<(usize, usize), HashSet<usize>>,
+    // nodes_to_edges: HashMap<(usize, usize), HashSet<usize>>,
 }
 
 impl Graph {
@@ -57,11 +106,17 @@ impl Graph {
             edges: vec![],
             address_to_node: HashMap::new(),
             address_to_edge: HashMap::new(),
+            adjacency: HashMap::new(),
+            // nodes_to_edges: HashMap::new(),
         }
     }
 }
 
 impl Graph {
+
+    pub fn get_edge(&self, index: usize) -> Result<&Edge> {
+        self.edges.get(index).context("Wrong Index")
+    }
     fn insert_node(&mut self, token: TokenInfo) -> Result<usize> {
         let token_address = Pubkey::from_str(&token.address.unwrap())?;
 
@@ -78,6 +133,7 @@ impl Graph {
         let index = self.nodes.len();
         self.nodes.push(node);
         self.address_to_node.insert(token_address, index);
+        self.adjacency.insert(index, HashSet::new());
 
         Ok(index)
     }
@@ -88,10 +144,11 @@ impl Graph {
         node0_index: usize,
         node1_index: usize,
     ) -> Result<usize> {
-        let (token_vault_lowest, token_vault_highest) = if node0_index < node1_index {
-            (pool.token_vault_a.unwrap(), pool.token_vault_b.unwrap())
+
+        let (token_vault_lowest, token_vault_highest, idx_lowest, idx_highest, reversed) = if node0_index < node1_index {
+            (pool.token_vault_a.unwrap(), pool.token_vault_b.unwrap(), node0_index, node1_index, false)
         } else {
-            (pool.token_vault_b.unwrap(), pool.token_vault_a.unwrap())
+            (pool.token_vault_b.unwrap(), pool.token_vault_a.unwrap(), node1_index, node0_index, true)
         };
         let address = Pubkey::from_str(&pool.address.unwrap())?;
         let edge = Edge {
@@ -103,6 +160,11 @@ impl Graph {
             token_vault_lowest: Pubkey::from_str(&token_vault_lowest)?,
             token_vault_highest: Pubkey::from_str(&token_vault_highest)?,
             config: Pubkey::from_str(&pool.config.unwrap())?,
+            node_lowest: idx_lowest,
+            node_highest: idx_highest,
+            decimals_lowest: self.nodes[idx_lowest].decimals,
+            decimals_highest: self.nodes[idx_highest].decimals,
+            reversed,
             sqrt_price: None,
             liquidity: None,
             current_tick_index: None,
@@ -111,6 +173,9 @@ impl Graph {
         let index = self.edges.len();
         self.edges.push(edge);
         self.address_to_edge.insert(address, index);
+
+        self.adjacency.get_mut(&idx_lowest).unwrap().insert(index);
+        self.adjacency.get_mut(&idx_highest).unwrap().insert(index);
 
         Ok(index)
     }
@@ -161,6 +226,157 @@ impl Graph {
         info!("Amount of Nodes in the Graph: {:?}", graph.nodes.len());
         Ok(graph)
     }
+
+
+    pub fn detect_cycles(&self, max_length: usize) -> Result<()> {
+        let mut all_cycles: HashSet<Vec<usize>> = HashSet::new();
+
+        let number_of_nodes = self.nodes.len();
+
+        for i in 0..number_of_nodes {
+            self.dfs_search(i, i, vec![i], max_length).into_iter().for_each(|x: Vec<usize>| {
+                if !x.is_empty() {
+                    all_cycles.insert(x);
+                }
+            });
+        }
+        // info!("Detected Cycles: {:?}", all_cycles);
+        info!("Number of Cycles: {:?}", all_cycles.len());
+
+        all_cycles.iter().for_each(move |cycle| {
+            if cycle.len() == 2 {
+                info!("Cycle: {:?}", cycle);
+            }
+        });
+        Ok(())
+    }
+
+
+
+    fn dfs_search(&self, start_node: usize, current_node: usize, mut visited_edges: Vec<usize>, max_depth: usize) -> HashSet<Vec<usize>>{
+        let mut cycles: HashSet<Vec<usize>> = HashSet::new();
+        if visited_edges.len() >= max_depth {
+            return cycles;
+        }
+        for neighbour_edge in self.adjacency.get(&current_node).unwrap() {
+
+            if visited_edges.contains(neighbour_edge) { continue; }
+
+            let other_node = self.edges.get(*neighbour_edge).unwrap().get_other_node(current_node).unwrap();
+
+
+            visited_edges.push(*neighbour_edge);
+
+            if other_node == start_node && visited_edges.len() >= 1 && visited_edges.len() <= max_depth {
+                cycles.insert(Self::canonicalize(visited_edges.clone()));
+            }
+
+            self.dfs_search(start_node, other_node, visited_edges.clone(), max_depth).into_iter().for_each(|x: Vec<usize>| {
+                if !x.is_empty() {
+                    cycles.insert(x);
+                }
+            });
+
+            visited_edges.pop();
+
+
+
+        }
+        return cycles;
+    }
+
+
+    fn canonicalize(mut cycle: Vec<usize>) -> Vec<usize> {
+        if cycle.is_empty() {
+            return cycle;
+        }
+
+        let min_value_index = cycle
+            .iter()
+            .enumerate()
+            .min_by_key(|&(_, val)| val)
+            .map(|(i, _)| i)
+            .unwrap();
+
+        cycle.rotate_left(min_value_index);
+        cycle
+    }
+
+    // pub fn detect_cycles(&self, max_length: usize) -> Result<Vec<Vec<usize>>> {
+    //     let number_of_nodes = self.nodes.len();
+    //     let mut dist = vec![f64::INFINITY; number_of_nodes];
+    //     let mut parent: Vec<Option<usize>> =  vec![None; number_of_nodes];
+    //     dist[0] = 0.0;
+
+    //     for _ in 0..(number_of_nodes-1) {
+    //         for edge in &self.edges {
+    //             if dist[edge.node_highest] != f64::INFINITY && dist[edge.node_lowest] > dist[edge.node_highest] + edge.get_log_exchange_rate(false) {
+    //                 dist[edge.node_lowest] = dist[edge.node_highest] + edge.get_log_exchange_rate(false);
+    //                 parent[edge.node_lowest] = Some(edge.node_highest);
+    //             }
+
+    //             if dist[edge.node_lowest] != f64::INFINITY && dist[edge.node_highest] > dist[edge.node_lowest] + edge.get_log_exchange_rate(true) {
+    //                 dist[edge.node_highest] = dist[edge.node_lowest] + edge.get_log_exchange_rate(true);
+    //                 parent[edge.node_highest] = Some(edge.node_lowest);
+    //             }
+
+    //         }
+
+    //     }
+
+    //     let mut cycles: Vec<Vec<usize>> = vec![];
+
+
+    //     for edge in &self.edges {
+    //         if  dist[edge.node_highest] != f64::INFINITY && dist[edge.node_lowest] > dist[edge.node_highest] + edge.get_log_exchange_rate(false) {
+    //             info!("Detected Negative Cycle");
+    //             dist[edge.node_lowest] = dist[edge.node_highest] + edge.get_log_exchange_rate(false);
+    //             parent[edge.node_lowest] = Some(edge.node_highest);
+
+    //             let mut x = edge.node_lowest;
+    //             for _ in 0..number_of_nodes {x = parent[x].unwrap()}
+
+    //             let mut cycle = vec![x];
+    //             let mut y = parent[x].unwrap();
+    //             let mut cycle_length = 1;
+    //             while y != x {
+    //                 if cycle_length > max_length { break; }
+    //                 cycle.push(y);
+    //                 cycle_length += 1;
+    //                 y = parent[y].unwrap();
+    //             }
+    //             if cycle_length <= max_length {
+    //                 cycle.sort();
+    //                 cycles.push(cycle);
+    //             }
+    //         }
+
+    //         if dist[edge.node_lowest] != f64::INFINITY && dist[edge.node_highest] > dist[edge.node_lowest] + edge.get_log_exchange_rate(true) {
+    //             info!("Detected Negative Cycle");
+    //             dist[edge.node_highest] = dist[edge.node_lowest] + edge.get_log_exchange_rate(true);
+    //             parent[edge.node_highest] = Some(edge.node_lowest);
+
+    //             let mut x = edge.node_highest;
+    //             for _ in 0..number_of_nodes {x = parent[x].unwrap()}
+
+    //             let mut cycle = vec![x];
+    //             let mut y = parent[x].unwrap();
+    //             let mut cycle_length = 1;
+    //             while y != x {
+    //                 if cycle_length > max_length { break; }
+    //                 cycle.push(y);
+    //                 cycle_length += 1;
+    //                 y = parent[y].unwrap();
+    //             }
+    //             if cycle_length <= max_length {
+    //                 cycle.sort();
+    //                 cycles.push(cycle);
+    //             }
+    //         }
+
+    //     }
+    //     Ok(cycles)
+    // }
 }
 
 #[cfg(test)]
